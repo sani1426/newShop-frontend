3f6165634faae71872c038c39b646fb76d8e9821
saman <sam.karimi727@gmail.com>
2025-05-19 00:38:10 +0330

    update ThemeToggle.jsx

--- components/themeToggle/ThemeToggle.jsx
+++ components/themeToggle/ThemeToggle.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const ThemeToggle = () => {
   return (
-    <div className='w-[40px] h-[20px] relative flex-between rounded-[50px] cursor-pointer bg-black px-1'>
+    <div className='w-[40px] h-[20px] relative flex-between rounded-[50px] cursor-pointer bg-sky-300 px-1 transition-all'>
       <Image src='/icons/moon.png' alt='moon' width={14} height={14} />
       <div className='w-[15px] h-[15px] rounded-full absolute left-[1px] bg-white'></div>
       <Image src='/icons/sun.png' alt='moon' width={14} height={14} />

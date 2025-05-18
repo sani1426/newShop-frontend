c40393e418aae83acb9c49ed238cb04eedda5e10
saman <sam.karimi727@gmail.com>
2025-05-19 01:40:02 +0330

    update page.jsx and page.jsx

--- app/(auth)/sign-up/page.jsx
+++ app/(auth)/sign-up/page.jsx
@@ -56,7 +56,7 @@ const page = () => {
   return (
     <section className='w-full h-screen grid md:grid-cols-2 overflow-hidden'>
       <div className='w-full h-full flex items-center justify-center'>
-        <div className='bg-slate-100 dark:bg-slate-900 shadow-md w-full h-full py-10 px-10'>
+        <div className='bg_soft shadow-md w-full h-full py-10 px-10'>
           <div className='w-full text-center border-b-2 pb-8 '>
             <h1 className='text-3xl'>Sign Up</h1>
           </div>

6818077e0fb2566767bbeaf442c2f991080d9b28
saman <sam.karimi727@gmail.com>
2025-05-19 01:36:18 +0330

    update 3 files and delete 1 file

--- components/themeToggle/ThemeToggle.jsx
+++ components/themeToggle/ThemeToggle.jsx
@@ -9,7 +9,7 @@ const ThemeToggle = () => {
   return (
     <div onClick={toggleTheme} className={`w-[40px] h-[20px] relative flex-between rounded-[50px] cursor-pointer px-1 transition-all ${theme === "light" ? "bg-sky-200" : "bg-blue-700"}`}>
       <Image src='/icons/moon.png' alt='moon' width={14} height={14} />
-      <div className={`w-[15px] h-[15px] rounded-full absolute  bg-white ${theme === 'light' ? "left-[1px]" : "right-[1px]"}`}></div>
+      <div className={`w-[15px] h-[15px] rounded-full absolute transition-all  bg-white ${theme === 'light' ? "left-[1px]" : "right-[1px]"}`}></div>
       <Image src='/icons/sun.png' alt='moon' width={14} height={14} />
     </div>
   )

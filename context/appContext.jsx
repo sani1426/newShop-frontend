bfafa7ca0572fa1d017074ef3bdeb9763427b31c
saman <sam.karimi727@gmail.com>
2025-05-19 00:53:57 +0330

    update appContext.jsx

--- context/appContext.jsx
+++ context/appContext.jsx
@@ -1,50 +1,42 @@
-"use client"
+'use client'
 
-
-
-import SummaryApi from "@/common"
-import useFetchData from "@/hooks/useFetchData"
-import { createContext, useContext, useEffect, useState } from "react"
+import SummaryApi from '@/common'
+import useFetchData from '@/hooks/useFetchData'
+import { createContext, useContext, useEffect, useState } from 'react'
 
 const AppContext = createContext()
 
+export const AppContextProvider = ({ children }) => {
+  const getFromLocal = () => {
+    if (typeof window !== undefined) {
+      const value = localStorage.getItem('theme')
 
+      return value || 'light'
+    }
+  }
 
+  const [user, setUser] = useState()
+  const [theme, setTheme] = useState(() => {
+    return getFromLocal()
+  })
 
-export const AppContextProvider = ({children}) => {
+  const getUserDetails = async () => {
+    const { result } = await useFetchData(SummaryApi.UserDetail.url, 'get')
 
-    const getFromLocal = ()=>{
-    
-        if(typeof window !== undefined){
-        const value = localStorage.getItem("theme");
-        
-        return value || "light"
-    }}
+    if (result) {
+      setUser(result)
+    }
+  }
 
-    const [user , setUser]= useState()
-    const [theme , setTheme]=useState(()=> {
-        return getFromLocal()
-    })
+  useEffect(() => {
+    getUserDetails()
+  }, [])
 
-    const getUserDetails = async () => {
-        const { result } = await useFetchData(SummaryApi.UserDetail.url, 'get')
-    
-        if (result) {
-          setUser(result)
-        }
-      }
-    
-      useEffect(() => {
-        getUserDetails()
-      }, [])
-
-    return (
-        <AppContext.Provider value={{user , setUser , theme}}>
-            {children}
-        </AppContext.Provider>
-    )
+  return (
+    <AppContext.Provider value={{ user, setUser, theme }}>
+      {children}
+    </AppContext.Provider>
+  )
 }
 
-
-export const useAppContext =()=>useContext(AppContext)
-
+export const useAppContext = () => useContext(AppContext)

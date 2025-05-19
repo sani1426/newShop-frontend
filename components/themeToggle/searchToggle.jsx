c71c61bc930794c76cb1b4de9f047843efbd3903
saman <sam.karimi727@gmail.com>
2025-05-19 23:43:08 +0330

    update 1 file, create 6 files and delete 1 file

--- components/themeToggle/searchToggle.jsx
+++ components/themeToggle/searchToggle.jsx
@@ -5,19 +5,19 @@ import { useState } from "react"
 const SearchToggle = () => {
     const [open , setOpen]=useState(false)
   return (
-    <>
+    <div className="relative">
       <button className=' bg-transparent border-2 px-3 py-2 rounded-md text-2xl '>
         <IoSearchOutline />
       </button>
 
       {
         open && (
-            <div className="w-20 h-8 bg_soft ">
+            <div className="w-20 h-8 bg_soft absolute py-2 px-4">
                 <input className="w-full py-2 bg-gray-300 " type="text" placeholder="سرچ کنید" />
             </div>
         )
       }
-    </>
+    </div>
   )
 }
 

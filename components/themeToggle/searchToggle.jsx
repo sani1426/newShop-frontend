33be50c3428448c5bedeae219126e4a88780169c
saman <sam.karimi727@gmail.com>
2025-05-20 00:15:34 +0330

    update searchToggle.jsx

--- components/themeToggle/searchToggle.jsx
+++ components/themeToggle/searchToggle.jsx
@@ -6,14 +6,14 @@ import { IoSearchOutline } from 'react-icons/io5'
 const SearchToggle = () => {
     const [open , setOpen]=useState(false)
   return (
-    <div className="relative">
+    <div className="">
       <button onClick={()=>setOpen(!open)} className=' bg-transparent border-2 px-3 py-2 rounded-md text-2xl '>
         <IoSearchOutline />
       </button>
 
       {
         open && (
-            <div className="w-20 h-8 bg_soft absolute py-2 px-4">
+            <div className="w-full h-8 bg_soft absolute py-2 px-4">
                 <input className="w-full py-2 bg-gray-300 " type="text" placeholder="سرچ کنید" />
             </div>
         )

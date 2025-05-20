932c9b23ceb74bedef1d3e2ce1c762e98031c00c
saman <sam.karimi727@gmail.com>
2025-05-21 00:14:53 +0330

    update MovieCard.jsx

--- components/shared/MovieCard.jsx
+++ components/shared/MovieCard.jsx
@@ -6,17 +6,17 @@ import { MdInfoOutline } from 'react-icons/md'
 
 const MovieCard = ({movie}) => {
   return (
-   <div className='hero'>
-    <div className="herobg">
-        <img className='hero-bg-image' src={movie?.movieImage} alt={movie?.name}/>
-        <div className="overlay"></div>
+   <div className='flex flex-col px-10 pt-20 gap-10 max-w-xl'>
+    <div className="absolute top-0 left-0 -z-10 h-screen w-screen">
+        <img className='object-cover w-full h-full' src={movie?.movieImage} alt={movie?.name}/>
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black-1"></div>
     </div>
 
-    <h1 className='hero-title'>{movie?.name}</h1>
-    <p className='hero-overview'>{movie?.description}</p>
+    <h1 className='text-white text-2xl '>{movie?.name}</h1>
+    <p className='text-white text-sm'>{movie?.description}</p>
 
-    <div className="hero-btns">
-        <button className='hero-btn'><MdInfoOutline /> More Info</button>
+    <div className="">
+        <button className='flex-center gap-1.5 px-4 py-3 rounded-xl cursor-pointer bg-white text-xl'><MdInfoOutline /> More Info</button>
     </div>
    </div>
   )

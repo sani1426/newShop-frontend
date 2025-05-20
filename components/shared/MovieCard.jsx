5a226e135e9e835e311a69599c5333d9dc84f92e
saman <sam.karimi727@gmail.com>
2025-05-21 00:16:38 +0330

    update MovieCard.jsx

--- components/shared/MovieCard.jsx
+++ components/shared/MovieCard.jsx
@@ -9,7 +9,7 @@ const MovieCard = ({movie}) => {
    <div className='flex flex-col px-10 pt-20 gap-10 max-w-xl'>
     <div className="absolute top-0 left-0 -z-10 h-screen w-screen">
         <img className='object-cover w-full h-full' src={movie?.movieImage} alt={movie?.name}/>
-        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black-1"></div>
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#131116]"></div>
     </div>
 
     <h1 className='text-white text-2xl '>{movie?.name}</h1>

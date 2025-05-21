2dc0975649d617aff436bcb34860fa28a11e35c3
saman <sam.karimi727@gmail.com>
2025-05-22 02:11:37 +0330

    update ThemeToggle.jsx

--- components/themeToggle/ThemeToggle.jsx
+++ components/themeToggle/ThemeToggle.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 
 const ThemeToggle = () => {
   const { theme, toggleTheme } = useAppContext()
-  console.log(theme)
+  
   return (
     <div
       onClick={toggleTheme}

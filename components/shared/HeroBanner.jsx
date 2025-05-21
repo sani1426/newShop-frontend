fbeaa93716ac53e279b02c03cffb676b6cfdaa3c
saman <sam.karimi727@gmail.com>
2025-05-22 00:05:11 +0330

    update HeroBanner.jsx and MobileNav.jsx

--- components/shared/HeroBanner.jsx
+++ components/shared/HeroBanner.jsx
@@ -13,13 +13,24 @@ const HeroBanner = ({bannerMovies}) => {
 
                 return(
                   <div className="min-w-full min-h-[450px] lg:min-h-full overflow-hidden relative">
-                    <div className="w-full h-full relative">
-                      <Image src={item?.backdrop} className='h-full object-cover w-full' fill priority alt={item?.name} />
+                    <div className="w-full h-full ">
+                      <img src={item?.backdrop} className='h-full object-cover w-full'  alt={item?.name} />
                     </div>
 
                     <div className="absolute top-0  w-full h-full bg-gradient-to-t from-slate-800 to-transparent"></div>
 
-                    <div className="container mx-auto absolute bottom-0 max-w-md"></div>
+                    <div className="container w-full mx-auto absolute bottom-0 max-w-md px-3">
+                      <h2 className='font-bold text-2xl'>{item?.name}</h2>
+                      <p className="text-ellipsis line-clamp-3  my-2">{item?.description}</p>
+
+                      <div className="">
+                        <p>امتیاز : {item?.rating}+</p>
+                        {/* <p>view</p> */}
+                      </div>
+                      <button className='bg-white px-4 py-2 text-black font-bold rounded mt-4'>
+                        Play Now
+                      </button>
+                    </div>
 
                   </div>
                 )
--- components/shared/MobileNav.jsx
+++ components/shared/MobileNav.jsx
@@ -9,14 +9,14 @@ const MobileNav = () => {
 
   return (
     <>
-      <div className='lg:hidden w-full fixed bg-opacity-75  bottom-0  bg_soft '>
+      <div className='lg:hidden py-3 w-full fixed bg-opacity-75  bottom-0  bg_soft '>
         <div className='flex items-center justify-between h-full transition-all duration-500'>
           {navItem.map((item) => (
             <Link
               key={item?.id}
               href={item?.href}
               className={`px-3 flex h-full items-center flex-col justify-center hover:text-grown-50 transition-all duration-300 ${
-                pathname === item?.href && 'text-grown-100 transform -translate-y-3'
+                pathname === item?.href && 'text-grown-100 '
               }`}
             >
               <div className='text-2xl'>{item?.icon}</div>

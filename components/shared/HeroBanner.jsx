34da76fae5eebbd3b652a6a507cdb8711f69b2ff
saman <sam.karimi727@gmail.com>
2025-05-22 01:05:45 +0330

    update HeroBanner.jsx

--- components/shared/HeroBanner.jsx
+++ components/shared/HeroBanner.jsx
@@ -38,6 +38,7 @@ const HeroBanner = ({ bannerMovies }) => {
                   className='h-full object-cover w-full'
                   alt={item?.name}
                   priority
+                  loading='lazy'
                 />
               </div>
 

d17d1ebdeee1263a41aec29946534ef0d433f26b
saman <sam.karimi727@gmail.com>
2025-05-21 23:05:15 +0330

    update 1 file, create 1 file and delete 1 file

--- app/(Root)/page.jsx
+++ app/(Root)/page.jsx
@@ -1,29 +1,15 @@
-import MovieCard from "@/components/shared/MovieCard";
+
+import HeroBanner from "@/components/shared/HeroBanner";
+import Movies from "@/data/movies";
 
 
 
 export default function Home() {
-  const movie =  {
-    name: 'Legends of the Condor Heroes: The Gallants 2025',
-    genres: ['اکشن' , 'درام' , 'ماجراجویی'],
-    year : '2025',
-    backdrop: '/images/movies/Legends of the Condor Heroes-backdrop.webp',
-    movieImages: ['/images/movies/Legends of the Condor Heroes.webp' , '/images/movies/Legends of the Condor Heroes-1.webp'],
-    rating: 6.0,
-    duration: 146,
-    language : 'Mandarin',
-    ageGroup: 'R',
-    Release : '1403/11/10',
-    director: 'Hark Tsui',
-    Stars : ['Zhan Xiao' , 'Tony Ka Fai Leung'],
-    likes : 34 ,
-    disLikes : 57,
-    description: 'افسانه‌ ی قهرمانان کوندور: دلیران، فیلمی اکشن ماجراجویی درام تاریخی عاشقانه جنگی به کارگردانی تسوی هارک (Tsui Hark) است. در قرن دوازدهم، در میانهٔ جنگ‌های خونین میان مغول‌ها به رهبری چنگیز خان و سلسله جین، «گو جینگ» (با بازی شیاو ژان) که در کودکی توسط مغول‌ها بزرگ شده، به دنبال معشوقه‌اش «هوانگ رونگ» (با بازی ژوانگ دافِی) میگردد. تا اینکه او …',
-  }
+
 
   return (
 <>
-<MovieCard movie={movie} />
+<HeroBanner bannerMovies={Movies} />
 </>
   );
 }
---
+++ components/shared/HeroBanner.jsx
@@ -0,0 +1,32 @@
+
+
+import Image from 'next/image'
+import React from 'react'
+
+
+const HeroBanner = ({bannerMovies}) => {
+  return (
+    <section className='w-full h-full'>
+        <div className="flex min-h-full max-h-[95vh]">
+            {
+              bannerMovies.map((item , index) => {
+
+                return(
+                  <div className="min-w-full min-h-[450px] lg:min-h-full overflow-hidden relative">
+                    <div className="w-full h-full relative">
+                      <Image src={item?.backdrop} className='h-full object-cover' fill priority alt={item?.name} />
+                    </div>
+
+                    <div className="absolute top-0  w-full h-full bg-gradient-to-t from-slate-800 to-transparent"></div>
+
+                    
+                  </div>
+                )
+              })
+            }
+        </div>
+    </section>
+  )
+}
+
+export default HeroBanner

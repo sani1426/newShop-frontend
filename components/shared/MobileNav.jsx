4ee3d0d24a56e9616d1f07e6c2af30842ec9f869
saman <sam.karimi727@gmail.com>
2025-05-22 21:36:55 +0330

    update MobileNav.jsx

--- components/shared/MobileNav.jsx
+++ components/shared/MobileNav.jsx
@@ -10,12 +10,12 @@ const MobileNav = () => {
   return (
     <>
       <div className='lg:hidden py-3 w-full fixed bg-opacity-75  bottom-0  bg_soft '>
-        <div className='flex items-center justify-between md:justify-evenly h-full transition-all duration-500'>
+        <div className='flex items-center justify-evenly h-full transition-all duration-500'>
           {navItem.map((item) => (
             <Link
               key={item?.id}
               href={item?.href}
-              className={` flex h-full items-center flex-col justify-center hover:text-grown-50 transition-all duration-300 bg_soft p-4 rounded-full ${
+              className={` flex h-full items-center flex-col justify-center hover:text-grown-50 transition-all duration-300  p-4 rounded-full ${
                 pathname === item?.href && 'text-grown-100  '
               }`}
             >

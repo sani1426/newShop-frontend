6e46dfe3df05267a7c651e00cce101cfebb013fe
saman <sam.karimi727@gmail.com>
2025-05-23 04:23:21 +0330

    update GenreSlider.jsx

--- section/GenreSlider.jsx
+++ section/GenreSlider.jsx
@@ -15,8 +15,8 @@ const GenresSlider = async ({ genre, header }) => {
 
   return (
     <div className='container  mx-auto'>
-      <h1 className='text-xl font-bold lg:text-2xl mb-3'>{header}</h1>
-      <div className='w-full grid  grid-cols-3 lg:grid-cols-5 gap-6'>
+      <h1 className='text-2xl font-bold lg:text-4xl mb-5'>{header}</h1>
+      <div className='w-full  flex items justify-between gap-6'>
         {Movies.map((item, index) => {
           return <Card key={index} data={item} />
         })}

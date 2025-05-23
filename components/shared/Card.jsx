a9076eaf6a5df3c8dc79f3bb119190a78477bea9
saman <sam.karimi727@gmail.com>
2025-05-24 00:07:43 +0330

    update Card.jsx and GenreSlider.jsx

--- components/shared/Card.jsx
+++ components/shared/Card.jsx
@@ -4,14 +4,17 @@ import Image from "next/image"
 
 const Card = ({data}) => {
   return (
-    <div className="w-full max-w-[230px] h-80 shadow-lg rounded-md relative">
+    <div className="w-full max-w-[230px] h-80 shadow-lg rounded-md ">
+      <div className="relative w-full h-[300px] ">
+
         <Image width={230} height={300} className="object-cover rounded-md" src={data?.movieImages[1]} alt={data?.name} />
-<div className="absolute bg-black/10 px-3 py-2 bottom-6 backdrop-blur-sm w-full flex-between">
+<div className="absolute bg-black/10 px-3 py-2 bottom-0 backdrop-blur-sm w-full flex-between">
   <p className="tex-sm text-white">{data?.rating}+</p>
   <p className="text-sm text-white">{data?.duration}min</p>
 </div>
-        <div className="w-full px-2 py-3 text-ellipsis line-clamp-1 text-center ">
-          <h2 className="text-xl font-bold">{data?.name}</h2>
+      </div>
+        <div className="w-full px-2 py-3 ">
+          <h2 className="text-xl font-bold  text-ellipsis line-clamp-1 text-center text-nowrap">{data?.name}</h2>
         </div>
     </div>
   )
--- section/GenreSlider.jsx
+++ section/GenreSlider.jsx
@@ -1,5 +1,5 @@
 import SummaryApi from '@/common'
-import Card from '@/components/shared/Card'
+// import Card from '@/components/shared/Card'
 import axios from 'axios'
 
 const GenresSlider = async ({ genre, header }) => {
@@ -16,9 +16,23 @@ const GenresSlider = async ({ genre, header }) => {
   return (
     <div className='container  mx-auto'>
       <h1 className='text-2xl font-bold lg:text-3xl mb-5'>{header}</h1>
-      <div className='w-full flex-center gap-5 flex-wrap'>
-        {Movies.map((item, index) => {
-          return <Card key={index} data={item} />
+      <div className='w-full grid grid-cols-3 lg:grid-cols-5 gap-6'>
+        {Movies.map((data, index) => {
+          return (
+            <div key={index} className="w-full max-w-[230px] h-80 shadow-lg rounded-md ">
+            <div className="relative w-full h-[300px] ">
+      
+              <Image width={230} height={300} className="object-cover rounded-md" src={data?.movieImages[1]} alt={data?.name} />
+      <div className="absolute bg-black/10 px-3 py-2 bottom-0 backdrop-blur-sm w-full flex-between">
+        <p className="tex-sm text-white">{data?.rating}+</p>
+        <p className="text-sm text-white">{data?.duration}min</p>
+      </div>
+            </div>
+              <div className="w-full px-2 py-3 ">
+                <h2 className="text-xl font-bold  text-ellipsis line-clamp-1 text-center text-nowrap">{data?.name}</h2>
+              </div>
+          </div>
+          )
         })}
       </div>
     </div>

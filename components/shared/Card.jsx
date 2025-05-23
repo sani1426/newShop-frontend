0a72f0d65b13e5e0f3b3ed730d45e8bb2bb54922
saman <sam.karimi727@gmail.com>
2025-05-23 03:50:41 +0330

    update Card.jsx and GenreSlider.jsx

--- components/shared/Card.jsx
+++ components/shared/Card.jsx
@@ -4,7 +4,7 @@
 const Card = ({data}) => {
   return (
     <div className="w-full max-w-[230px] h-80 overflow-hidden rounded ">
-        <img className="w-full h-full" src={data?.movieImages[0]} alt={data?.name} />
+        <img className="w-full h-full object-cover" src={data?.movieImages[1]} alt={data?.name} />
     </div>
   )
 }
--- section/GenreSlider.jsx
+++ section/GenreSlider.jsx
@@ -1,31 +1,26 @@
-import SummaryApi from "@/common"
-import Card from "@/components/shared/Card";
-import axios from "axios";
+import SummaryApi from '@/common'
+import Card from '@/components/shared/Card'
+import axios from 'axios'
 
+const GenresSlider = async ({ genre, header }) => {
+  let Movies = []
 
+  const { data } = await axios.get(
+    `${SummaryApi.getMoviesByGenre.url}?genre=${genre}`
+  )
 
-const GenresSlider =async  ({genre , header}) => {
-    let Movies = [] ;
-
-    const {data} = await  axios.get(`${SummaryApi.getMoviesByGenre.url}?genre=${genre}`)
-
-    if(data.success){
-        Movies = await data?.data
-    }
+  if (data.success) {
+    Movies = await data?.data
+  }
 
   return (
-    <div className="container  mx-auto">
-      <h1 className="text-xl font-bold lg:text-2xl mb-3">{header}</h1>
-      <div className="grid grid-cols-[repeat(auto-fit,230px)] gap-6">
-      {
-        Movies.map((item , index) => {
-          return(
-            <Card  key={index}  data={item} />
-          )
-        })
-      }
+    <div className='container  mx-auto'>
+      <h1 className='text-xl font-bold lg:text-2xl mb-3'>{header}</h1>
+      <div className='grid  grid-cols-[repeat(auto-fit,230px)] gap-6'>
+        {Movies.map((item, index) => {
+          return <Card key={index} data={item} />
+        })}
       </div>
-    
     </div>
   )
 }

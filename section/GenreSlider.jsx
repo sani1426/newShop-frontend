1158ab98216d48187f98cc2375fe7536d4a3b807
saman <sam.karimi727@gmail.com>
2025-05-24 01:51:03 +0330

    update GenreSlider.jsx

--- section/GenreSlider.jsx
+++ section/GenreSlider.jsx
@@ -2,6 +2,7 @@ import SummaryApi from '@/common'
 import Carousel from '@/components/shared/carousel'
 
 import axios from 'axios'
+import Link from 'next/link'
 
 
 
@@ -20,7 +21,13 @@ const GenresSlider = async ({ genre, header }) => {
 
   return (
     <div className='container  mx-auto'>
+      <div className='w-full flex-between'>
       <h1 className='text-2xl font-bold lg:text-3xl '>{header}</h1>
+      <Link href={`/`} className='  text-white dark:text-black px-4 py-3 rounded-sm '>
+      مشاهده بیشتر
+      </Link>
+      </div>
+      
       <section className='mt-8'>
     <Carousel data={Movies} />
     </section>

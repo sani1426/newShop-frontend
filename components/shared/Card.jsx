b18ed03a7f65d5b046127255b9b89cfd78a21fac
saman <sam.karimi727@gmail.com>
2025-05-24 01:46:11 +0330

    update Card.jsx

--- components/shared/Card.jsx
+++ components/shared/Card.jsx
@@ -1,14 +1,15 @@
+import Link from 'next/link';
 import { FaStar } from 'react-icons/fa';
 
 
 
 const Card = ({data}) => {
   return (
-    <div className='aspect-[2/3] bg-gray-300  relative rounded-md overflow-hidden'>
+    <Link href={`/movie/${data?._id}`} className='aspect-[2/3] bg-gray-300  relative rounded-md overflow-hidden'>
     <img src={data?.movieImages[1]} className='object-cover w-full h-full' alt='' />
-    <div className='Card_movie_Info'>
-    <h2 className='text-white'>{data?.name}</h2>
-  <div className='flex gap-3 mt-2'>
+    <div dir='ltr' className='Card_movie_Info'>
+    <h2 className='text-white text-nowrap text-ellipsis line-clamp-1'>{data?.name}</h2>
+  <div  className='flex gap-3 mt-2'>
     <span className='text-yellow-400 text-lg'>
       <FaStar />
     </span>
@@ -16,7 +17,7 @@ const Card = ({data}) => {
   </div>
     </div>
 
-</div>
+</Link>
 );
 
 }

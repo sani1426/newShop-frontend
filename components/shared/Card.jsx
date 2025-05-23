f1146d1effde5bf3cc80dc0f29a07610de28181b
saman <sam.karimi727@gmail.com>
2025-05-23 04:46:41 +0330

    update Card.jsx

--- components/shared/Card.jsx
+++ components/shared/Card.jsx
@@ -3,7 +3,7 @@
 
 const Card = ({data}) => {
   return (
-    <div className="w-full max-w-[230px] h-80 overflow-hidden rounded-md relative">
+    <div className="w-full max-w-[230px] h-80 overflow-hidden shadow-lg rounded-md relative">
         <img className="w-full h-full object-cover rounded-md" src={data?.movieImages[1]} alt={data?.name} />
 <div className="absolute bottom-5 w-full flex-between">
   <p>{data?.reating}+</p>

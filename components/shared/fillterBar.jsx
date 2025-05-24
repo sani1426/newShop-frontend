f2a2bdc03ad2e01e24487a7ed0146b8f3e302c18
saman <sam.karimi727@gmail.com>
2025-05-24 23:02:27 +0330

    update globals.css and fillterBar.jsx

--- components/shared/fillterBar.jsx
+++ components/shared/fillterBar.jsx
@@ -5,15 +5,15 @@ import React from 'react'
 const FillterBar = () => {
   return (
    <section className='w-full bg_soft py-8 px-12'>
-    <div className="mx auto">
+    <div className="mx auto bg_main rounded-md">
         <form className='flex-center gap-6' >
             
-            <select  >
+            <select   className='px-4 py-3 border-2 focus:border-grown-100 rounded-md'>
                 <option >فیلم/سریال</option>
                 <option value="movie">فیلم</option>
                 <option value="tv">سریال</option>
             </select>
-            <select  >
+            <select   className='px-4 py-3 border-2 focus:border-grown-100 rounded-md  >
                 <option >انتخاب ژانر</option>
                 <option value="اکشن">اکشن</option>
                 <option value="عاشقانه">عاشقانه</option>
@@ -25,7 +25,7 @@ const FillterBar = () => {
                 <option value="ترسناک">ترسناک</option>
                 <option value="کمدی">کمدی</option>
             </select>
-            <select  >
+            <select   className='px-4 py-3 border-2 focus:border-grown-100 rounded-md >
                 <option >مرتب سازی</option>
                 <option value="محبوب ترین">محبوب ترین ها</option>
                 <option value="بالاترین امتیاز">بالاترین امتیاز</option>

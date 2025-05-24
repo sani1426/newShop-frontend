76871a063b86184aebd8a92973bbcf58b996a61a
saman <sam.karimi727@gmail.com>
2025-05-25 00:20:00 +0330

    update HomeCarousel.jsx

--- components/shared/HomeCarousel.jsx
+++ components/shared/HomeCarousel.jsx
@@ -22,37 +22,37 @@ let autoNext = setTimeout(() => {
   nextBtn.click();
 }, timeAutoNext);
 
-// Next/prev button click events
+
 nextBtn.onclick = () => showSlider('next');
 prevBtn.onclick = () => showSlider('prev');
 
-// Reset progress bar animation
+
 function resetTimeAnimation() {
   runningTime.style.animation = 'none';
   runningTime.offsetHeight; // trigger reflow
   runningTime.style.animation = 'runningTime 7s linear 1 forwards';
 }
 
-// Handle carousel slide logic
+
 function showSlider(direction) {
   const sliderItems = list.querySelectorAll('.carousel .list .item');
 
   if (direction === 'next') {
-    list.appendChild(sliderItems[0]); // move first to last
+    list.appendChild(sliderItems[0]); 
     carousel.classList.add('next');
   } else {
-    list.prepend(sliderItems[sliderItems.length - 1]); // move last to first
+    list.prepend(sliderItems[sliderItems.length - 1]); 
     carousel.classList.add('prev');
   }
 
-  // Remove animation class after slide transition
+
   clearTimeout(runTimeout);
   runTimeout = setTimeout(() => {
     carousel.classList.remove('next');
     carousel.classList.remove('prev');
   }, timeRunning);
 
-  // Reset auto next timer
+
   clearTimeout(autoNext);
   autoNext = setTimeout(() => {
     nextBtn.click();
@@ -61,7 +61,7 @@ function showSlider(direction) {
   resetTimeAnimation();
 }
 
-// Start animation on load
+
 resetTimeAnimation();
 
   
@@ -71,22 +71,22 @@ resetTimeAnimation();
 
 
   return (
-    <div class='carousel'>
-      <div class='list'>
+    <div className='carousel'>
+      <div className='list'>
         {Movies.map((item) => (
           <div
-            class='item'
-            style='background-image: url(https://www.jqueryscript.net/dummy/1.jpg)'
+            className='item'
+            style={{background : `url(${item?.backdrop})`}}
           >
-            <div class='content'>
-              <div class='title'>SLIDER</div>
-              <div class='name'>One</div>
-              <div class='des'>
+            <div className='content'>
+              <div className='title'>SLIDER</div>
+              <div className='name'>One</div>
+              <div className='des'>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                 Officiis culpa similique consequuntur, reprehenderit dicta
                 repudiandae.
               </div>
-              <div class='btn'>
+              <div className='btn'>
                 <button>See More</button>
                 <button>Subscribe</button>
               </div>
@@ -94,12 +94,12 @@ resetTimeAnimation();
           </div>
         ))}
 
-        <div class='arrows'>
+        <div className='arrows'>
           <button class='prev'>prev</button>
           <button class='next'>next</button>
         </div>
 
-        <div class='timeRunning'></div>
+        <div className='timeRunning'></div>
       </div>
     </div>
   )

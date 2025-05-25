96451564e1394f0689cf878a26cc68c7023a6b1b
saman <sam.karimi727@gmail.com>
2025-05-26 00:48:51 +0330

    update 1 file and create 25 files

--- app/(Root)/movie/[id]/page.jsx
+++ app/(Root)/movie/[id]/page.jsx
@@ -1,41 +1,104 @@
-import SummaryApi from "@/common"
-import axios from "axios"
+import SummaryApi from '@/common'
+import Card from '@/components/shared/Card'
+import axios from 'axios'
+import { FaStar } from 'react-icons/fa'
+import { RiPokerHeartsFill } from 'react-icons/ri'
+import { FaRegHeart } from 'react-icons/fa'
+import { GiShare } from 'react-icons/gi'
 
+const page = async ({ params }) => {
+  let details
+  let similarMovies
+  const { id } = await params
 
+  const { data } = await axios.get(`${SummaryApi.getMovieDetails.url}/${id}`)
+  if (data.success) {
+    details = data?.data
+  }
 
-const page = async ({params}) => {
-  let details ;
-  let similarMovies  ;
-    const {id} = await params
-
-    const {data} = await axios.get(`${SummaryApi.getMovieDetails.url}/${id}`)
-    if(data.success){
-      details = data?.data
-    }
-
-    const response = await axios.post(`${SummaryApi.getSimilarMovies.url}` , {
-      movieId : id
-    })
-    const result = await response?.data
-    if(result.success){
-      similarMovies = result?.data
-    }
+  const response = await axios.post(`${SummaryApi.getSimilarMovies.url}`, {
+    movieId: id,
+  })
+  const result = await response?.data
+  if (result.success) {
+    similarMovies = result?.data
+  }
 
   return (
-    <div className="pt-20 px-5">
-        <h1 className="text-3xl text-red-600 mb-5 ">{details?.name}</h1>
-        <p>{details?.description}</p>
+    <>
+      {details ? (
+        <>
+          <header className='h-[100vh] '>
+            <div
+              className='absolute h-[80%] w-full flex justify-end pb-2 flex-col gap-8 pl-8'
+              style={{
+                background: `linear-gradient(to bottom,rgba(0,0,0,.6) , rgba(0,0,0,.8)) ,url('${details?.backdrop}')`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+              }}
+              alt={details?.name}
+            >
+              <h1 className='text-white  text-3xl'>{details?.name}</h1>
 
-        <div>
-          <ul>
-            {
-              similarMovies.map(item => (
-                <li key={item?._id}>{item?.name}</li>
-              ))
-            }
-          </ul>
+              <div className='flexCenter mt-4 gap-12 w-[45%] border-t-2 border-b-2 py-2 border-slate-400 border-opacity-50 bg-gradient-to-r from-[rgba(0,0,0,.5)] to-[rgba(0,0,0,.5)] rounded-md '>
+                <div className=' flex'>
+                  {/* <button
+                  className={`${
+                    isFavorite ? "text-green-600" : " text-red-800"
+                  }  py-2  rounded-md text-xl flexCenter gap-2`}
+                >
+                  <span
+                    className={` px-2 py-2 border-2  rounded-full primary`}
+                  >
+                    {isFavorite ? <RiPokerHeartsFill /> : <FaRegHeart />}{" "}
+                  </span>
+                  {isFavorite ? "remove from favorite" : "Add To favorit"}
+                </button> */}
+                  <button className='px-6 py-2 text-red-800 rounded-md text-xl flexCenter gap-2'>
+                    <span className='px-2 py-2 border-2 border-red-800 rounded-full'>
+                      <GiShare />
+                    </span>
+                    اشتراک گذاری
+                  </button>
+                </div>
+              </div>
+              <div className='flex items-center gap-8 text-3xl  w-[55%] border-b-2 border-opacity-50  border-slate-400 '>
+                <div className='flexCenter gap-2 border-slate-400 border-opacity-50  border-r-2 pr-4 pb-2'>
+                  <span className='text-yellow-400 text-4xl '>
+                    <FaStar />
+                  </span>
+                  <div className='flex flex-col text-[.9rem] text-center'>
+                    <span>
+                      {details?.rating}{' '}
+                      <span className='text-slate-400'>/ 10</span>
+                    </span>
+                    {/* <span className="text-slate-500 text-sm"> {movie.vote_count} Reviews</span> */}
+                  </div>
+                </div>
+
+                {/* <span className="flex  gap-2">
+               <span className="text-[1rem] text-slate-400">Rate This Movie :</span>
+                <CustomizedRating movieId={movie?.id}  />
+                </span> */}
+              </div>
+              <div className='w-[50%] bg-gradient-to-r from-[rgba(0,0,0,.5)] to-[rgba(0,0,0,.5)] rounded-md py-1'>
+                <p>{details?.description}</p>
+              </div>
+            </div>
+
+            <div className='w-[300px] h-[400px] absolute left-[70%] top-[13rem]'>
+              <Card data={details} />
+            </div>
+          </header>
+        </>
+      ) : (
+        <div className='v-[100vh] w-full'>
+          <div className='absolute bg-slate-400 animate-pulse h-[90%] w-full flex justify-end pb-2 flex-col gap-8 pl-8'></div>
+          <div className='w-[300px] h-[400px] absolute left-[70%] top-[13rem] bg-slate-500 animate-pulse rounded-lg'></div>
         </div>
-    </div>
+      )}
+    </>
   )
 }
 

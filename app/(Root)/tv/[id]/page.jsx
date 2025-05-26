4fc6ec9be6dda8b69dad4a05712e1bb0775569e1
saman <sam.karimi727@gmail.com>
2025-05-27 02:28:27 +0330

    update page.jsx

--- app/(Root)/tv/[id]/page.jsx
+++ app/(Root)/tv/[id]/page.jsx
@@ -30,7 +30,7 @@ const page = async ({ params }) => {
 
   return (
     <>
-      <header dir='ltr' className='h-[100vh] '>
+     <header dir='ltr' className='h-[100vh] '>
         <div className='relative h-[100%] w-full flex justify-end pb-2 flex-col gap-8 pl-8 pt-24'>
           <Image
             src={details?.backdrop}
@@ -42,7 +42,7 @@ const page = async ({ params }) => {
             fill
             loading='eager'
           />
-          <div className='w-full h-full absolute inset-0 bg-gradient-to-t from-black/75 to-transparent z-10'></div>
+          <div className='w-full h-full absolute inset-0 bg-gradient-to-t from-black/55 to-black/50 z-10'></div>
 
           <div className='z-20 absolute bottom-0 lg:right-4 lg:top-[6rem] lg:w-[50%] px-4'>
             <h1 className='text-3xl font-bold text-white'>{details?.name}</h1>
@@ -102,9 +102,7 @@ const page = async ({ params }) => {
               </div>
             </div>
           </div>
-
-
-          <div className='hidden lg:block w-[300px] h-[400px] absolute left-[70%] top-[13rem] z-20'>
+          <div className='hidden lg:block w-[400px] h-[500px] absolute left-[3%] top-[4rem] z-20'>
             <Card data={details} />
           </div>
         </div>
@@ -119,50 +117,3 @@ const page = async ({ params }) => {
 
 export default page
 
-/* <div className='z-20'>
-<h1 className='text-white  text-3xl '>{details?.name}</h1>
-
-<div className='flexCenter mt-4 gap-12 w-[45%] border-t-2 border-b-2 py-2 border-slate-400 border-opacity-50 bg-gradient-to-r from-[rgba(0,0,0,.5)] to-[rgba(0,0,0,.5)] rounded-md  '>
-<div className=' flex '>
-
-<button className='px-6 py-2 text-red-800 rounded-md text-xl flex-center gap-2'>
-<span className='px-2 py-2 border-2 border-red-800 rounded-full'>
-<GiShare />
-</span>
-اشتراک گذاری
-</button>
-<div className='flex-center gap-5'>
-<button className='px-4 py-2 border-1 border-slate-200 text-grown-100'> <span>({details?.likes})</span>دوست داشتم</button>
-<button className='px-4 py-2 border-slate-200 border-1 text-red-500'>دوست نداشتم <span>({details?.disLikes})</span></button>
-</div>
-</div>
-</div>
-<div className='flex items-center gap-8 text-3xl  w-[55%] border-b-2 border-opacity-50  border-slate-400  bg-gradient-to-r from-[rgba(0,0,0,.5)] to-[rgba(0,0,0,.5)]'>
-<div className='flex-center gap-2 border-slate-400 border-opacity-50  border-r-2 pr-4 pb-2'>
-<span className='text-yellow-400 text-4xl '>
-<FaStar />
-</span>
-<div className='flex flex-col text-[.9rem] text-center text-slate-100'>
-<span>
-{details?.rating}{' '}
-<span className='text-slate-400'>/ 10</span>
-</span>
-{/* <span className="text-slate-500 text-sm"> {movie.vote_count} Reviews</span> 
-</div>
-</div>
-<div className='flex-center gap-4 text-white'>
-<div className='flex-center gap-2'>
-<h1>کارگردان :</h1>
-<h2>{details?.director}</h2>
-</div>
-<div className='flex-center gap-2'>
-<h1>ستارگان :</h1>
-<h2>{details?.stars}</h2>
-</div>
-</div>
-</div>
-<div className='w-[50%] bg-gradient-to-r from-[rgba(0,0,0,.5)] to-[rgba(0,0,0,.5)] rounded-md py-1'>
-<p className='text-slate-100'>{details?.description}</p>
-</div>
-</div>
-    </div> */

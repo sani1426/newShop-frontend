54ac73b18ccf8f9205ffdcd927cb54292491e9b3
saman <sam.karimi727@gmail.com>
2025-05-27 01:02:34 +0330

    update Footer.jsx

--- components/shared/Footer.jsx
+++ components/shared/Footer.jsx
@@ -6,7 +6,7 @@ import { FiPhoneCall } from "react-icons/fi";
 
 const Footer = () => {
   return (
-    <footer className='bg-slate-100 text-slate-900 dark:bg-slate-800 dark:text-slate-100 border-t border-t-slate-800 dark:border-t-slate-200 py-12 px-10'>
+    <footer className='bg_main border-t border-t-slate-800 dark:border-t-slate-200 py-12 px-10'>
       <div className='container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-4 lg:px-0'>
         <div>
           <h3 className='text-lg text-slate-800 dark:text-slate-100 mb-4'>

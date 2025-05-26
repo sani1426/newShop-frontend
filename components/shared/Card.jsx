fc9aadfd9dbfb536f54ffc1fa515343ebd5ec42e
saman <sam.karimi727@gmail.com>
2025-05-26 22:23:27 +0330

    update Card.jsx and homeCarousel.css

--- components/shared/Card.jsx
+++ components/shared/Card.jsx
@@ -5,7 +5,7 @@ import { FaStar } from 'react-icons/fa';
 
 const Card = ({data}) => {
   return (
-    <Link href={`/${data?.category}/${data?._id}`} className='aspect-[2/3] bg-gray-300  relative rounded-md overflow-hidden'>
+    <Link href={`/${data?.category}/${data?._id}`} className='aspect-[2/3] bg-gray-300  relative rounded-md overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-500'>
     <img src={data?.movieImages[1]} className='object-cover w-full h-full rounded-md' alt='' />
     <div dir='ltr' className='Card_movie_Info'>
     <h2 className='text-white text-nowrap text-ellipsis line-clamp-1'>{data?.name}</h2>

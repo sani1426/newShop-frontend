e75af06cbe0d4aa480ab25fe2eb335fc2f37871b
saman <sam.karimi727@gmail.com>
2025-05-28 00:41:31 +0330

    update Card.jsx

--- components/shared/Card.jsx
+++ components/shared/Card.jsx
@@ -10,7 +10,7 @@ const Card = ({data}) => {
     <div className="flip-card-inner">
       <div className="flip-card-front">
       <img alt="" className="w-full h-full object-cover" src={data?.movieImages[1]}  />
-         <div dir='ltr' className='Card_movie_Info'>
+         <div dir='ltr' className='Card_movie_Info z-10'>
       <h2 className='text-white text-nowrap text-ellipsis line-clamp-1'>{data?.name}</h2>
     <div  className='flex gap-3 mt-2'>
       <span className='text-yellow-400 text-lg'>

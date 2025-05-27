5246d6507377754c5ed52e919e7aa7729e9a780b
saman <sam.karimi727@gmail.com>
2025-05-28 01:37:22 +0330

    update AnimationCard.jsx

--- components/shared/AnimationCard.jsx
+++ components/shared/AnimationCard.jsx
@@ -1,13 +1,14 @@
 import Link from 'next/link'
 import './homeCarousel.css'
 import { FaStar } from 'react-icons/fa'
+import Image from 'next/image'
 
 const AnimationCard = ({data}) => {
   return (
     <Link href={`/${data?.category}/${data?._id}`}  className="flip-card rounded-lg aspect-[2/3]">
     <div className="flip-card-inner rounded-lg">
       <div className="flip-card-front rounded-lg">
-      <img alt="" className="w-full h-full object-cover" src={data?.movieImages[1]}  />
+      <Image alt="" className="w-full h-full object-cover" src={data?.movieImages[1]} fill  />
          <div dir='ltr' className='Card_movie_Info z-10'>
       <h2 className='text-white text-nowrap text-ellipsis line-clamp-1'>{data?.name}</h2>
     <div  className='flex gap-3 mt-2'>
@@ -20,6 +21,25 @@ const AnimationCard = ({data}) => {
       </div>
       <div className="flip-card-back">
       <img alt="" className="w-full h-full object-cover" src={data?.movieImages[0]}  />
+      <div dir='ltr' className='Card_movie_Info z-10'>
+      <div
+              dir='rtl'
+              className='flex items-center mt-4  gap-2 w-full justify-start'
+            >
+              <h2 className='text-white'>ژانرها :</h2>
+              <div className='flex-center gap-4'>
+                {data?.genres.map((genre, index) => (
+                  <Link
+                    className='px-3 py-1 border-[1px] rounded-md border-white text-white text-sm'
+                    key={index}
+                    href='/'
+                  >
+                    {genre}
+                  </Link>
+                ))}
+              </div>
+            </div>
+    </div>
       </div>
     </div>
   </Link>

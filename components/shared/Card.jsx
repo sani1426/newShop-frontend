9abaab7c22e7679bd0e6f162ffc6a99e977ea624
saman <sam.karimi727@gmail.com>
2025-05-27 23:21:05 +0330

    update Card.jsx and animatedCart.jsx

--- components/shared/Card.jsx
+++ components/shared/Card.jsx
@@ -1,13 +1,15 @@
 import Link from 'next/link';
 import { FaStar } from 'react-icons/fa';
-
+import './homeCarousel.css'
+import Image from 'next/image';
 
 
 const Card = ({data}) => {
   return (
-    <Link href={`/${data?.category}/${data?._id}`} className='aspect-[2/3] bg-gray-300  relative rounded-md overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-500'>
-    <img src={data?.movieImages[1]} className='object-cover w-full h-full rounded-md' alt='' />
-    <div dir='ltr' className='Card_movie_Info'>
+    <Link  href={`/${data?.category}/${data?._id}`} className="anime-card w-[190px] h-[254px] transition-all duration-500 rounded-[10px] ">
+      <div className="first-content relative">
+       <Image alt="" width={1000} height={1000} className="w-full h-full object-cover" src={data?.movieImages[1]}  />
+       <div dir='ltr' className='Card_movie_Info'>
     <h2 className='text-white text-nowrap text-ellipsis line-clamp-1'>{data?.name}</h2>
   <div  className='flex gap-3 mt-2'>
     <span className='text-yellow-400 text-lg'>
@@ -16,10 +18,33 @@ const Card = ({data}) => {
     <span className='text-white'>{`${data?.rating} / 10`}</span>
   </div>
     </div>
-
-</Link>
+      </div>
+      <div className="second-content relative">
+      <Image alt="" width={1000} height={1000} className="w-full h-full object-cover" src={data?.movieImages[1]} />
+    <span className="bg-white z-10">saman</span>
+      </div>
+    
+    
+    </Link>
 );
 
 }
 
 export default Card
+
+
+
+
+// <Link href={`/${data?.category}/${data?._id}`} className='aspect-[2/3] bg-gray-300  relative rounded-md overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-500'>
+// <img src={data?.movieImages[1]} className='object-cover w-full h-full rounded-md' alt='' />
+// <div dir='ltr' className='Card_movie_Info'>
+// <h2 className='text-white text-nowrap text-ellipsis line-clamp-1'>{data?.name}</h2>
+// <div  className='flex gap-3 mt-2'>
+// <span className='text-yellow-400 text-lg'>
+//   <FaStar />
+// </span>
+// <span className='text-white'>{`${data?.rating} / 10`}</span>
+// </div>
+// </div>
+
+// </Link>
--- components/shared/animatedCart.jsx
+++ components/shared/animatedCart.jsx
@@ -1,23 +1,14 @@
-import Image from "next/image"
-import './homeCarousel.css'
+// import Image from "next/image"
+// import './homeCarousel.css'
+// import Link from "next/link"
 
 
 
-const AnimatedCart = () => {
-  return (
+// const AnimatedCart = () => {
+//   return (
   
-    <div className="anime-card w-[190px] h-[254px] transition-all duration-500 rounded-[10px] ">
-      <div className="first-content">
-       <Image alt="" width={1000} height={1000} className="w-full h-full object-cover" src='/images/movies/Black Panther Wakanda Forever 2022-1.jpg' />
-      </div>
-      <div className="second-content">
-      <Image alt="" width={1000} height={1000} className="w-full h-full object-cover" src='/images/movies/Black Panther Wakanda Forever 2022-1.jpg z-[-1]' />
-    <span className="bg-white z-10">saman</span>
-      </div>
-    
-    
-    </div>
-  )
-}
+ 
+//   )
+// }
 
-export default AnimatedCart
+// export default AnimatedCart

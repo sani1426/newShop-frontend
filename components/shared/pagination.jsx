01ca967816e4b29d7928987a3b58d65585bf1240
saman <sam.karimi727@gmail.com>
2025-05-29 00:30:17 +0330

    update pagination.jsx

--- components/shared/pagination.jsx
+++ components/shared/pagination.jsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 function PageChanging({category , totalDocument}) {
     const router = useRouter()
   const [currentPage, setCurrentPage] = useState(1);
+  const [totalAmount , setTotalAmount]=useState(Math.ceil(totalDocument/8))
 
   const onPageChange = (page) => {
     setCurrentPage(page);
@@ -17,7 +18,7 @@ function PageChanging({category , totalDocument}) {
   },[currentPage])
   return (
     <div dir="ltr" className="flex overflow-x-auto sm:justify-center mt-8">
-      <Pagination dir="ltr" currentPage={currentPage} totalPages={totalDocument/8} onPageChange={onPageChange} showIcons />
+      <Pagination dir="ltr" currentPage={currentPage} totalPages={totalAmount} onPageChange={onPageChange} showIcons />
     </div>
   );
 }

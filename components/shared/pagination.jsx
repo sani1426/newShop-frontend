4e7e63f6eaed91c4258425b317be114fb1e70d18
saman <sam.karimi727@gmail.com>
2025-05-28 23:24:11 +0330

    update pagination.jsx

--- components/shared/pagination.jsx
+++ components/shared/pagination.jsx
@@ -15,7 +15,7 @@ function PageChanging() {
   }
   return (
     <div dir="ltr" className="flex overflow-x-auto sm:justify-center">
-      <Pagination currentPage={currentPage} totalPages={4} onPageChange={onPageChange} showIcons />
+      <Pagination dir="ltr" currentPage={currentPage} totalPages={4} onPageChange={onPageChange} showIcons />
     </div>
   );
 }

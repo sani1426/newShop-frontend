050f0700c8e6702bc2d533912c594bccd785f935
saman <sam.karimi727@gmail.com>
2025-05-28 20:46:46 +0330

    update searchToggle.jsx

--- components/themeToggle/searchToggle.jsx
+++ components/themeToggle/searchToggle.jsx
@@ -9,11 +9,11 @@ import { useState } from 'react'
 import { IoSearchOutline } from 'react-icons/io5'
 
 const SearchToggle = () => {
-  const {serchResult , setSearchResult} = useAppContext()
+  const { setSearchResult} = useAppContext()
   const pathname = usePathname()
   const router = useRouter()
   const [open, setOpen] = useState(false)
-  const [searchvalue , setSearchValue] = useState()
+  const [searchvalue , setSearchValue] = useState('')
 
   const handleSearch = async (e) => {
     setSearchValue(prev => prev + e.target.value)
@@ -41,7 +41,7 @@ const SearchToggle = () => {
 
         <div className={`w-full lg:w-[500px] h-[5rem] flex-center bg_soft absolute py-2 px-8 lg:px-5 top-[5.5rem] left-0 rounded-md transition-all duration-500  ${open ? "opacity-1 pointer-events-auto" : 'opacity-0 pointer-events-none'} `}>
           <input
-          value={(e)=>searchvalue(e)}
+          value={searchvalue}
           onChange={handleSearch}
             className='w-full rounded-md px-4 py-2 bg-gray-300 '
             type='text'

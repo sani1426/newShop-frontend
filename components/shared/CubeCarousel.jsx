d21e5deac4b64135d6da6f1b3bde64468bc0197a
saman <sam.karimi727@gmail.com>
2025-05-29 14:14:00 +0330

    update CubeCarousel.jsx

--- components/shared/CubeCarousel.jsx
+++ components/shared/CubeCarousel.jsx
@@ -1,23 +1,28 @@
 "use client"
 
-import react from 'react '
 import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/styles.css';
-import withAutoplay from 'react-awesome-slider/dist/autoplay'
+import AwesomeSliderStyles from 'react-awesome-slider/src/styled/cube-animation.scss';
 
 const AutoplaySlider = withAutoplay(AwesomeSlider);
 const CubeCarousel = () => {
     return(
 
-        <AutoplaySlider
-        play={true}
-        cancelOnInteraction={false} // should stop playing on user interaction
-        interval={6000}
-      >
-    <div data-src="/images/movies/Black Panther Wakanda Forever 2022-backdrop.webp" />
-    <div data-src="/images/movies/code8-backdrop.webp" />
-    <div data-src="/images/movies/Doctor Strange in the Multiverse of Madness 2022-backdrop.webp" />
-    </AutoplaySlider>
+
+
+        
+        
+        
+
+   
+        
+     
+          <AwesomeSlider cssModule={AwesomeSliderStyles}>
+            <div data-src="/path/to/image-0.jpg" />
+            <div data-src="/path/to/image-1.jpg" />
+            <div data-src="/path/to/image-2.jpg" />
+            <div data-src="/path/to/image-3.jpg" />
+          </AwesomeSlider>
+        
     )
 }
 

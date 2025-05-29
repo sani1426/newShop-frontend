a6f7421758a1360e4f69c3fa2bd7e0f86309dcb3
saman <sam.karimi727@gmail.com>
2025-05-29 22:21:40 +0330

    update CubeCarousel.jsx

--- components/shared/CubeCarousel.jsx
+++ components/shared/CubeCarousel.jsx
@@ -1,8 +1,14 @@
 "use client"
 
 import AwesomeSlider from 'react-awesome-slider';
-import AwesomeSliderStyles from 'react-awesome-slider/src/styled/cube-animation.scss';
+import withCaption from 'react-awesome-slider/dist/captioned';
+import 'react-awesome-slider/dist/styles.css';
+import 'react-awesome-slider/dist/captioned.css';
 
+
+      
+const CaptionedSlider = withCaption(AwesomeSlider);
+       
 
 const CubeCarousel = () => {
     return(
@@ -15,13 +21,26 @@ const CubeCarousel = () => {
 
    
         
-     
-          <AwesomeSlider cssModule={AwesomeSliderStyles}>
-            <div data-src="/path/to/image-0.jpg" />
-            <div data-src="/path/to/image-1.jpg" />
-            <div data-src="/path/to/image-2.jpg" />
-            <div data-src="/path/to/image-3.jpg" />
-          </AwesomeSlider>
+   
+ 
+  
+        <CaptionedSlider
+          startupScreen={StartupScreen}
+          cssModule={CaptionedStyles}
+          screens={[
+            {
+              backgroundColor: '#4a9c8c',
+              media: '/images/movies/Black Panther Wakanda Forever 2022-backdrop.webp',
+              caption: 'I want to see what you got.',
+            },
+            {
+              backgroundColor: '#4a9c8c',
+              media: '/images/movies/code8-backdrop.webp',
+              caption: "The answer is -- Don't think about it.",
+            },
+          ]}
+        />
+      
         
     )
 }

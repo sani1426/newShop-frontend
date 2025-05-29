d7c409a395e1d6da20c0c26b23875b7bb43525cb
saman <sam.karimi727@gmail.com>
2025-05-29 13:52:18 +0330

    update CubeCarousel.jsx

--- components/shared/CubeCarousel.jsx
+++ components/shared/CubeCarousel.jsx
@@ -3,15 +3,15 @@
 
 
 import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+import 'react-awesome-slider/dist/custom-animations/cubeAnimation.css';
 
 const CubeCarousel = () => {
     return(
 
   <AwesomeSlider animation="cubeAnimation">
-    <div data-src="/images/movies/Black Panther Wakanda Forever 2022-backdrop.webp" />
-    <div data-src="/images/movies/code8-backdrop.webp" />
-    <div data-src="/images/movies/Doctor Strange in the Multiverse of Madness 2022-backdrop.webp" />
+    <div data-src="/path/to/images/movies/Black Panther Wakanda Forever 2022-backdrop.webp" />
+    <div data-src="/path/to/images/movies/code8-backdrop.webp" />
+    <div data-src="/path/to/images/movies/Doctor Strange in the Multiverse of Madness 2022-backdrop.webp" />
   </AwesomeSlider>
     )
 }

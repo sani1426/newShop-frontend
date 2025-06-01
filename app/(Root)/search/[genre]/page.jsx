7a9689626be8929e2cf1d823904abcafd16a9746
saman <sam.karimi727@gmail.com>
2025-06-02 00:03:26 +0330

    update page.jsx

--- app/(Root)/search/[genre]/page.jsx
+++ app/(Root)/search/[genre]/page.jsx
@@ -9,7 +9,7 @@ import axios from 'axios'
 import React from 'react'
 
 const page = async ({params}) => {
-    const {genre} = params
+    const {genre} = await params
     let Movies ;
     let total ;
 
